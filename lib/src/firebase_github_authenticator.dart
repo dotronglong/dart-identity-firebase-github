a6922f23768acbcaf6713219eb9be86197a5a69b
Lucas <me@dotronglong.com>
2020-04-26 21:08:39 +0800

    v0.3.0

--- lib/src/firebase_github_authenticator.dart
+++ lib/src/firebase_github_authenticator.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:github_sign_in/github_sign_in.dart';
 import 'package:identity/identity.dart';
-import 'package:sso/sso.dart';
 
 class FirebaseGithubAuthenticator
     with WillNotify, WillConvertUser
